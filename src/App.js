afc71c1689667e2be0b0810ce35d8d68e1a975ec
student <valiev.ilnur@bk.ru>
2020-03-24 00:26:47 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import TodoList from './TodoList'
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
+export default function App() {
+
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Купить хлеб и молоко', completed: true },
+    { id: 2, title: 'Оплатить коммуналку', completed: false },
+    { id: 3, title: 'Забрать долг у Васи', completed: false },
+  ])
+
+  const [todoTitle, setTodoTitle] = useState('')
+
+  const addTodo = ev => {
+    if (ev.key === 'Enter') {
+      setTodos([
+        ...todos,
+        {
+          id: todos[todos.length - 1].id + 1,
+          title: todoTitle,
+          completed: false
+        }
+      ])
+      setTodoTitle('')
+    }
   }
 
-  render() {
-    return (
-      <div className="container">
-        <h1>Todo app</h1>
+  return (
+    <div className="container">
+      <h4>Список дел</h4>
 
-          <div className="input-field">
-            <input type="text" />
-            <label>Todo name</label>
-          </div>
-
-          <TodoList todos={this.state.todos} />
+      <div className="input-field">
+        <input type="text" value={todoTitle} onChange={ev => setTodoTitle(ev.target.value)} onKeyPress={addTodo} />
+        {/* <button onClick={addTodo} >Добавить</button> */}
+        <label>Введите задачу</label>
       </div>
-    );
-  }
+
+      <TodoList todos={todos} />
+    </div>
+  );
 }
